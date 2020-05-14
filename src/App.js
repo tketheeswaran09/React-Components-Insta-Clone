3b5098939d4b49a2a44029bd6a789ca3d2e248ef
Tharshan Ketheeswaran <tketheeswaran09@gmail.com>
2020-05-14 23:38:30 +0100

    Completed proj

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
-
+import PostPage from './components/PostsContainer/PostsPage';
+import SearchBar from './components/SearchBar/SearchBarContainer';
+// import dummy-data
+import dummyData from './dummy-data';
 
 const App = () => {
-
+  // set up state for the dummy data and pass to your PostsPage
+  const [data, setData] = useState(dummyData);
   return (
     <div className="App">
-      {/* Add imported components here to render them */}
+      <SearchBar />
+      <PostPage data={data} setData={setData} />
     </div>
   );
 };
 
 export default App;
+
--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -2,8 +2,6 @@
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
 
 const LikeSection = props => {
   return (
@@ -13,13 +11,16 @@ const LikeSection = props => {
         key="likes-icons-container"
       >
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
+          <i className="far fa-heart" onClick={() => props.setLikes((props.likes) + 1)} />
+          {/* <button onClick={()=>props.setLikes((props.likes) + 1)}></button> */}
         </div>
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faComment} />
+          <i className="far fa-comment" />
         </div>
       </div>
-      <p className="like-number">27 likes</p>
+      <p className="like-number">{props.likes}
+
+      likes</p>
     </div>
   )
 };
